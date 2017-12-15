3a14ac8b97a5dd4d180d378dc572a6818f5a78c3
Michael Friedrich <michael.friedrich@netways.de>
2017-12-15 11:50:28 +0100

    Update setup.py and remove custom builds

--- setup.py
+++ setup.py
@@ -1,27 +1,12 @@
 import os
-from distutils.command.build import build
 
-from django.core import management
 from setuptools import setup, find_packages
-
 
 try:
     with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
 except:
     long_description = ''
-
-
-class CustomBuild(build):
-    def run(self):
-        management.call_command('compilemessages', verbosity=1, interactive=False)
-        build.run(self)
-
-
-cmdclass = {
-    'build': CustomBuild
-}
-
 
 setup(
     name='pretix-invoice-net',
@@ -33,10 +18,10 @@ setup(
     author_email='support@netways.de',
     license='Apache Software License',
 
+    # pretix already depends on invoice related packages (reportlab, etc.)
     install_requires=[],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
-    cmdclass=cmdclass,
     entry_points="""
 [pretix.plugin]
 pretix_invoice_net=pretix_invoice_net:PretixPluginMeta
