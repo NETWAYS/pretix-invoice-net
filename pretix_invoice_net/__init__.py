712be12eb473a4aea5c994a847fd12fa07d73182
Michael Friedrich <michael.friedrich@netways.de>
2019-09-03 16:54:05 +0200

    Release v1.0.1

--- pretix_invoice_net/__init__.py
+++ pretix_invoice_net/__init__.py
@@ -11,7 +11,7 @@ class PluginApp(AppConfig):
         author = 'NETWAYS GmbH'
         description = ugettext_lazy('Pretix invoice renderer plugin for NETWAYS')
         visible = True
-        version = '1.0.0'
+        version = '1.0.1'
 
     def ready(self):
         from . import signals  # NOQA
